34d639b0361fc29e1715090da196e3ecb28ad702
thesea58 <19501521.hai@student.iuh.edu.vn>
2022-11-10 09:45:44 +0700

    what

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates',static_folder='static')
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def hello():
-    render_template("static/index.html")
+    return render_template('index.html')
+if __name__ == "__main__":
+      app.run('0.0.0.0','80', debug=True)
